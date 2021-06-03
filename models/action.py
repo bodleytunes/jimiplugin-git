3ff6fe7bb5d1e088ddec9d7ad20e0f2eb70a5bf0
Jon Clayton <m87tech@gmail.com>
2021-06-03 12:16:44 +0100

    adding git clone to actino

--- models/action.py
+++ models/action.py
@@ -88,6 +88,66 @@ class _GitOps(action._action):
         return super(_GitOps, self).setAttribute(attr, value, sessionData=sessionData)
 
 
+class _GitClone(action._action):
+
+    repo: Repo = None
+    # the root of the git path, so the clone should be to a project name as a subpath of this
+    clone_git_path: str = "/tmp/git/backups"
+    clone_url_path: str = "https://github.com/bodleytunes/jimiplugin-batfish.git/"
+    clone_subpath: str = (
+        "jimiplugin-batfish"  # generally this should match the git project name
+    )
+
+    def doAction(self, data):
+        # pre flight checks
+        self._pre_flights(data)
+        # do cloning
+        g = Git(args=GitArgs(git_path=self.clone_git_path), CLONE=True)
+        self.repo = g.clone(
+            local_clone_path=self.clone_git_path,
+            url_path=self.clone_url_path,
+            clone_subpath=self.clone_subpath,
+        )
+        if self.repo is not None:
+            return {
+                "result": True,
+                "rc": 0,
+                "msg": "Git-Clone successfull",
+                "data": "Git Operations Complete!",
+                "errors": "",
+            }
+        else:
+            return {
+                "result": False,
+                "rc": 255,
+                "msg": "Clone failed",
+                "data": f"Path exists {self.clone_git_path}",
+                "errors": "Clone failed",
+            }
+
+    def _pre_flights(self, data):
+        self.clone_git_path = _helper.set_git_path(
+            git_path=self.clone_git_path, data=data
+        )
+
+    def _set_git_path(git_path: str, data=None) -> str:
+        # set the git path to the previously set destination folder if no explicit git path was passed in
+        try:
+            if data["eventData"]["backup_args"]["dst_folder"] is not None:
+                if git_path is None or git_path == "/tmp/git/backups":
+                    git_path = data["eventData"]["backup_args"]["dst_folder"]
+                    return git_path
+        except Exception as e:
+            print(f"{e}")
+            return git_path
+
+    def setAttribute(self, attr, value, sessionData=None) -> super:
+        # set parent class session data
+        return super(_cfgGitClone, self).setAttribute(
+            attr, value, sessionData=sessionData
+        )
+
+
 class _helper:
     def __init__(self) -> None:
         pass
